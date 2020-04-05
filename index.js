0f18ccc2878c3045c88272a60dcfd7082f1e6e9b
Sean <Sean.Harris@nordstrom.com>
2020-04-04 19:55:55 -0700

    - moved rendering code out into it's own lib - added "setTheme" to main index - added some helper control character commands

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Bar = require('./lib/Bar');
 const TodoList = require('./lib/TodoList');
 const Log = require('./lib/Log');
+const { setTheme } = require('./lib/render');
 
 const bar = options => new Bar(options);
 const log = () => new Log();
@@ -10,4 +11,5 @@ module.exports = {
 	bar,
 	log,
 	todoList,
+	setTheme,
 };
