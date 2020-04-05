dde7a6e06f8b657606cf63b0717e855735a31326
Sean <Sean.Harris@nordstrom.com>
2020-04-04 17:35:20 -0700

    - bars can now display a different char for the "current" position - components can override `get isAnimating` to ensure a refresh is happening if they need to animate frames - added support for themes

---
+++ themes/default.js
@@ -0,0 +1,19 @@
+const Bar = {
+	charIncomplete: '\u2591',
+	charComplete: '\u2593',
+	charCurrent: '',
+	format: '{bar} {percent}',
+};
+
+const TodoList = {
+	charTodo: '□',
+	charDone: '✔︎',
+	charFailed: '✕',
+	charPaused: '■',
+	charsProgress: '⣾⣽⣻⢿⡿⣟⣯⣷',
+};
+
+module.exports = {
+	Bar,
+	TodoList,
+};
